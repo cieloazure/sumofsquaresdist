6843eca00064336971b44e94ed0033c0126f7aa3
Akash S <akash.shingte@gmail.com>
2018-09-01 12:03:12 -0400

    Made assertions false instead of random check

--- test/sumofsquares_test.exs
+++ test/sumofsquares_test.exs
@@ -5,27 +5,27 @@ defmodule SumofsquaresTest do
   describe "Sumofsquares.distribute/2" do
     test "distributes the task among multiple cores" do
       # pending
-      assert 1+1 == 3
+      assert false
     end
 
     test "checks sum of squares is a perfect square" do
       # pending
-      assert 1+1 == 3
+      assert false
     end
 
     test "gives empty list in case no perfect squares exists for a sum of squares" do
       # pending
-      assert 1+1 == 3
+      assert false
     end
 
     test "arguments are valid" do
       # pending
-      assert 1+1 == 3
-    end
+      assert false
+   end
 
     test "arguments are invalid" do
       # pending
-      assert 1+1 == 3
+      assert false
     end
   end
 end
