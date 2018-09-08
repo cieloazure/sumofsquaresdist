0eb4aa438ccdc1b19469092677f2306d5f09a733
Akash S <akash.shingte@gmail.com>
2018-09-08 09:39:14 -0400

    Added documentation

--- lib/sumofsquares/result.ex
+++ lib/sumofsquares/result.ex
@@ -1,18 +1,51 @@
 defmodule Sumofsquares.Result do 
+  @moduledoc """
+  An agent to maintain the state of the results calculated by the subworkers
+  The state is a list which stores the elements
+  Defines a API for using agent functions
+  """
   use Agent
 
+  @doc """
+  Function to start the agent
+
+  ##Example
+  iex> result = Sumofsquares.Result.start_link([])
+  """
   def start_link(_opts) do
     Agent.start_link(fn -> [] end)
   end
 
+  @doc """
+  Function to insert the new result value in the agent
+
+  ##Example
+  iex> result = Sumofsquares.Result.start_link([])
+  iex> Sumofsquares.Result.put(result, 1) 
+  """
   def put(result, new_result_value) do
     Agent.update(result, fn old_results -> [new_result_value | old_results] end)
   end
 
+  @doc """
+  Function to insert multiple values in the agent
+
+  ##Example
+  iex> result = Sumofsquares.Result.start_link([])
+  iex> Sumofsquares.Result.put_bulk(result, [1,2]) 
+  """
   def put_bulk(result, new_result_values) do
     Agent.update(result, fn old_results -> new_result_values ++ old_results end)
   end
 
+  @doc """
+  Function to get the state of the agent
+
+  ##Example
+  iex> result = Sumofsquares.Result.start_link([])
+  iex> Sumofsquares.Result.put_bulk(result, [1,2,3,4]) 
+  iex> Sumofsquares.Result.get_result(result)
+  """
   def get_result(result) do
     Agent.get(result, fn results -> results end)
   end
--- lib/sumofsquares/subproblem_worker.ex
+++ lib/sumofsquares/subproblem_worker.ex
@@ -1,5 +1,35 @@
 defmodule Sumofsquares.SubproblemWorker do
+  @moduledoc """
+  A module to find the solution to the sum of squares problem
 
+  The problem states that given a range of numbers `n` we have to find
+  a sequence of `k` numbers from any number `i` in `n` such that sum of squares
+  of these numbers is a number who itself is perfect square
+  """
+  # TODO: Check if this module can be made to use the behaviour of Task
+  # TODO: Pass a function instead of an agent to update the state in order to
+  # decouple the behaviour
+
+  @doc """
+  A function to compute the solution to the sum of squares problem
+
+  Accepts lower bound, upper bound, sequence length and an agent to return the results as parameters
+  Returns `nil`
+  ##Example
+  iex> result = Sumofsquares.Result.start_link([])
+  iex> Sumofsquares.SubproblemWorker.solve(1, 40, 24, results)
+  """
+  def solve(lb, ub, k, agent) do
+    sol = Enum.map(lb..ub, fn num -> solve_unit_problem(num, num + k - 1) end)
+    |> Enum.filter(fn num -> !is_nil(num) end)
+    if !is_nil(sol), do: Sumofsquares.Result.put_bulk(agent, sol)
+  end
+
+  @doc """
+  A function to specify the child process specifications to the supervisor
+
+  Useful in case of starting a worker under a supervisor
+  """
   def child_spec([lb, ub, k]) do
     %{
       id: Sumofsquares.SubproblemWorker,
@@ -9,13 +39,11 @@ defmodule Sumofsquares.SubproblemWorker do
     }
   end
 
-  def solve(lb, ub, k, agent) do
-    sol = Enum.map(lb..ub, fn num -> solve_unit_problem(num, num + k - 1) end) 
-    |> Enum.filter(fn num -> !is_nil(num) end) 
-    Sumofsquares.Result.put_bulk(agent, sol)
-  end
-
-
+  # A function to solve one unit of problem
+  # Takes lower bound and upper bound as arguments
+  # And tells whether the sum of squares of sequence between lower bound and upper bound
+  # is a perfect square
+  # If it is a perfect square it returns the lower bound
   defp solve_unit_problem(lower_bound, upper_bound) do
     upper_sum = upper_bound * (upper_bound + 1) * (upper_bound * 2 + 1) / 6
 
