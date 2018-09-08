f04a5a469960329b1e1a5c3c2ab6e63d333387f2
Akash S <akash.shingte@gmail.com>
2018-09-08 09:41:13 -0400

    Added some test cases with some pending test cases

--- test/sumofsquares/result_test.exs
+++ test/sumofsquares/result_test.exs
@@ -18,4 +18,11 @@ defmodule Sumofsquares.ResultTest do
     Sumofsquares.Result.put(result, 1)
     assert Sumofsquares.Result.get_result(result) == [1]
   end
+
+  test "agent can concatanate two arrays using put_bulk", %{result: result} do
+    Sumofsquares.Result.put_bulk(result, [1,2,3,4])
+    assert Sumofsquares.Result.get_result(result) == [1,2,3,4]
+    Sumofsquares.Result.put_bulk(result, [5,6,7,8])
+    assert Sumofsquares.Result.get_result(result) == [5,6,7,8,1,2,3,4]
+  end
 end
