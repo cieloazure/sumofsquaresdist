3d19c9b0bf0ad4433cf9182747337e24a09a80d5
Akash S <akash.shingte@gmail.com>
2018-09-10 21:24:11 -0400

    Implemented boss with workers which won't die

--- lib/sumofsquares/boss.ex
+++ lib/sumofsquares/boss.ex
@@ -9,8 +9,9 @@ defmodule Sumofsquares.Boss do
   2) Server Callbacks - If defines the behaviour which is invoked when GenServer specific callbacks
   are initiated
   """
-  use GenServer
+  use GenServer, restart: :transient
   require Logger
+  @compile {:parse_transform, :ms_transform}
 
   # Constants Definition
 
@@ -18,13 +19,13 @@ defmodule Sumofsquares.Boss do
   # TODO: Find optimal number of workers
   # TODO: Check if the constant can be moved to runtime
   # Constant
-  @num_workers 50
+  @num_workers 200
 
   # Subproblem size for each worker
   # TODO: Find optimal subproblem size
   # TODO: Check if the constant can be moved to runtime
   # Constant
-  @subproblem_size 1000
+  @subproblem_size 100000
 
   # Start of Client API
 
@@ -40,35 +41,8 @@ defmodule Sumofsquares.Boss do
     GenServer.start_link(__MODULE__, :ok, opts)
   end
 
-  @doc """
-  A function to check whether sum of squares sequences of a specified length
-  starting from 1 upto a certain limit is a perfect square concurrently
-
-  Use GenServer.cast to create multiple processes without blocking the main thread
-
-  Returns a atom saying :ok if the cast was successful
-
-  #Example:
-  iex> {:ok, boss} = Sumofsquares.Boss.start_link(name: Boss)
-  iex> Sumofsquares.Boss.calculate(Boss, 1000000, 24)
-  """
-  def calculate(server, limit, sequence_length) do
-    GenServer.cast(server, {:solve, limit, sequence_length})
-  end
-
-  # TODO: Figure out a better way to return the results
-  @doc """
-  A function to get the results from the state saved in the Sumofsquares.Boss
-
-  Returns an array of results
-  #Example:
-  iex> {:ok, boss} = Sumofsquares.Boss.start_link(name: Boss)
-  iex> Sumofsquares.Boss.get_results(Boss)
-  """
-  def get_results(server) do
-    # refs should be empty
-    # next_subproblem_index > n
-    GenServer.call(server, {:result})
+  def calculate(n, k) do
+    GenServer.cast(Sumofsquares.Boss, {:solve_v2, n, k})
   end
 
   # End of Client API
@@ -84,10 +58,8 @@ defmodule Sumofsquares.Boss do
   Returns a tuple containing the state
   """
   def init(:ok) do
-    # IO.puts "initiating...."
-    # Process references
-    # State
-    refs = %{}
+    #workers table
+    workers = :ets.new(:workers, [:set, :protected])
 
     # Next  subproblem to be solved
     # State
@@ -103,154 +75,92 @@ defmodule Sumofsquares.Boss do
     # sequence length
     sequence_length = 0
 
-    {:ok, {refs, results, next_subproblem_index, limit, sequence_length}}
+    workers = spawn_subproblem_workers(workers)
+
+    {:ok, {workers, results, next_subproblem_index, limit, sequence_length}}
   end
 
-  @doc """
-  A GenServer callback to handle messages  with `:result` from the Client API
-
-  Returns the state of the result agent
-  """
-  def handle_call(
-        {:result},
-        _from,
-        {refs, results, next_subproblem_index, limit, sequence_length}
-      ) do
-    # IO.puts "in handle_call...."
-    {:reply, Sumofsquares.Result.get_result(results),
-     {refs, results, next_subproblem_index, limit, sequence_length}}
+  def handle_cast({:solve_v2, limit, sequence_length}, {workers, results, next_subproblem_index, _limit, _sequence_length}) do
+    {workers, results, next_subproblem_index} = distribute_subproblems(workers, results, next_subproblem_index, limit, sequence_length, nil)
+    {:noreply, {workers, results, next_subproblem_index, limit, sequence_length}}
   end
 
-  @doc """
-  A GenServer callback to handle messages with `:solve` from the Client API
-
-  Accepts the message with limit and sequence length as input and solves the problem
-  concurrently by spawning multiple workers and monitoring their processes
-  """
-  def handle_cast({:solve, n, k}, {refs, results, next_subproblem_index, limit, sequence_length}) do
-    Logger.info("In handle_cast...solving the problem")
-    limit = n
-    sequence_length = k
-
-    {refs, next_subproblem_index} =
-      spawn_workers(refs, results, next_subproblem_index, limit, sequence_length)
-
-    {:noreply, {refs, results, next_subproblem_index, limit, sequence_length}}
+  def handle_call({:get_results}, _from, {workers, results, next_subproblem_index, _limit, _sequence_length} = state) do
+    {:reply, Sumofsquares.Result.get_result(results),state}
   end
 
-  # TODO: Check :DOWN message reason to check if it exited in a normal way
-  @doc """
-  A Genserver callback to handle messages with `:DOWN` message from the processes when a process
-  exits
+  def handle_info({:execution_complete, p}, {workers, results, next_subproblem_index, limit, sequence_length}) do
+    #IO.puts "in execution complete"
+    #IO.inspect :ets.match_object(workers, {:_, :_, :_})
+    #IO.inspect p
 
-  This callback  will remove the process from the state of refs then will spawn additional workers if the problem is not yet solved
-  """
-  def handle_info(
-        {:DOWN, ref, :process, _pid, _reason},
-        {refs, results, next_subproblem_index, limit, sequence_length}
-      ) do
-    Logger.info("In handle_info: Got down message from a process.....")
-    {_v, refs} = Map.pop(refs, ref)
+    f = :ets.fun2ms(fn({ref, pid, _status}) when pid == p -> {ref, pid, :idle} end)
+    :ets.select_replace(workers, f)
 
-    Logger.debug(fn -> "In handle_info: Refs Map is of size " <> inspect(map_size(refs)) end)
-
-    Logger.debug(fn ->
-      "In handle_info: next subproblem index is " <> inspect(next_subproblem_index)
-    end)
-
-    {refs, next_subproblem_index} =
-      spawn_workers(refs, results, next_subproblem_index, limit, sequence_length)
-
-    cont = if next_subproblem_index > limit && map_size(refs) == 0, do: {:continue, :get_results}
-    Logger.debug(fn -> "In handle_info: Value of cont is " <> inspect(cont) end)
-
-    if !is_nil(cont) do
-      {:noreply, {refs, results, next_subproblem_index, limit, sequence_length}, cont}
+    {workers, results, next_subproblem_index} = if next_subproblem_index < limit do
+      distribute_subproblems(workers, results, next_subproblem_index, limit, sequence_length, p)
     else
-      {:noreply, {refs, results, next_subproblem_index, limit, sequence_length}}
+      {workers, results, next_subproblem_index}
+    end
+
+    w = :ets.match_object(workers, {:_, :_, :_})
+    #IO.inspect Enum.all?(w, fn worker -> elem(worker, 2) == :idle end)
+    #IO.inspect next_subproblem_index
+    #IO.inspect limit
+
+    if Enum.all?(w, fn worker -> elem(worker, 2) == :idle end) and next_subproblem_index > limit  do
+      IO.inspect Sumofsquares.Result.get_result(results)
+    end
+
+    {:noreply, {workers, results, next_subproblem_index, limit, sequence_length}}
+  end
+ 
+  defp spawn_subproblem_workers(workers) do
+    current_worker_table_size = length(:ets.match_object(workers, {:_, :_, :_}))
+    if current_worker_table_size < @num_workers do
+      pid = Process.spawn(Sumofsquares.SubproblemWorker, :loop_acceptor, [self()], [])
+      ref = Process.monitor(pid)
+      :ets.insert(workers, {ref, pid, :idle})
+      spawn_subproblem_workers(workers)
+    else
+      workers
     end
   end
 
-  # TODO: Define behaviour for messages other than `:DOWN`
-  @doc """
-  A GenServer callback to handle messages other than `:DOWN`
-  """
-  def handle_info(_msg, state) do
-    Logger.info("In other handle_info")
-    {:noreply, state}
+  defp distribute_subproblems(workers, results, next_subproblem_index, limit, sequence_length, pid) do
+    #IO.puts "distribute"
+    #IO.inspect :ets.match_object(workers, {:_, :_, :idle})
+    #IO.inspect pid
+    first_idle_worker= List.first(:ets.match_object(workers, {:_, :_, :idle}))
+    idle_worker =if !is_nil(first_idle_worker) do
+      pid || elem(first_idle_worker, 1)
+    else
+      nil
+    end
+
+    ##IO.puts "idle worker"
+    ##IO.inspect idle_worker
+
+    if !is_nil(idle_worker) and next_subproblem_index < limit do
+      lb = next_subproblem_index
+      ##IO.inspect lb
+      ub = min(next_subproblem_index + @subproblem_size - 1, limit)
+      ##IO.inspect ub
+      ##IO.inspect idle_worker
+      send_new_subproblem(idle_worker, lb, ub, sequence_length, results) 
+
+      f = :ets.fun2ms(fn({ref, pid, _status}) when pid == idle_worker -> {ref, pid, :busy} end)
+      :ets.select_replace(workers, f)
+
+      next_subproblem_index = min(next_subproblem_index + @subproblem_size, limit + 1)
+      distribute_subproblems(workers, results, next_subproblem_index, limit, sequence_length, nil)
+    else
+      {workers, results, next_subproblem_index}
+    end
   end
 
-  # TODO: Return results in a string form with each entry on a new line
-  @doc """
-  A GenServer callback to handle continues with message `:get_results`
-
-  Initiation of this callback signifies that there are no more subproblem to be solved
-  and results are ready to be returned
-  """
-  def handle_continue(
-        :get_results,
-        {_refs, results, _next_subproblem_index, _limit, _sequence_length} = state
-      ) do
-    Logger.info("Got to continue....Sending the results")
-    Logger.debug(fn -> inspect(Sumofsquares.Result.get_result(results)) end)
-    str_concat = fn s -> s <> "\nPress <Ctrl-C> to exit" end
-    Enum.sort(Sumofsquares.Result.get_result(results))
-    |> Enum.join("\n") 
-    |> str_concat.()
-    |> IO.puts
-    {:noreply, state}
-  end
-
-  # A private function to spawn wokers
-  # Checks for available spaces in workers and if there is a next subproblem to be solved
-  # If the conditions above are met it will spawn a worker, update the state of
-  # refs to indicate there is worker and also update the state of next_subproblem_index
-  defp spawn_workers(refs, results, next_subproblem_index, limit, sequence_length) do
-    Logger.info("In spawn_workers")
-    Logger.debug(fn -> "In spawn_workers: Size of ref map is " <> inspect(map_size(refs)) end)
-
-    Logger.debug(fn ->
-      "In spawn_workers: Next subproblem index is " <> inspect(next_subproblem_index)
-    end)
-
-    {_refs, _next_subproblem_index} =
-      if next_subproblem_index < limit and map_size(refs) < @num_workers do
-        Logger.info("In spawn_workers: spawing a worker...")
-
-        pid =
-          Process.spawn(
-            Sumofsquares.SubproblemWorker,
-            :solve,
-            [
-              next_subproblem_index,
-              min(next_subproblem_index + @subproblem_size - 1, limit),
-              sequence_length,
-              results
-            ],
-            []
-          )
-
-        ref = Process.monitor(pid)
-        refs = Map.put(refs, ref, pid)
-
-        Logger.debug(fn ->
-          "In spawn_workers: Updated Size of ref map is " <> inspect(map_size(refs))
-        end)
-
-        next_subproblem_index = min(next_subproblem_index + @subproblem_size, limit + 1)
-
-        Logger.debug(fn ->
-          "In spawn_workers: Updated value of next_subproblem_index is " <>
-            inspect(next_subproblem_index)
-        end)
-
-        spawn_workers(refs, results, next_subproblem_index, limit, sequence_length)
-      else
-        Logger.info(
-          "In spawn_workers: No more free workers available or no more subproblems remaining"
-        )
-
-        {refs, next_subproblem_index}
-      end
+  defp send_new_subproblem(pid, lb, ub, k, agent) do
+    ##IO.puts "sending new subproblem"
+    send(pid, {:solve_new_subproblem, lb, ub, k, agent})
   end
 end
